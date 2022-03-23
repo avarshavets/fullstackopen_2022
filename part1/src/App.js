67423a7f8f5f9fa81d73815e79c9db9c991382f2
Anna Varshavets <a.varshavets@gmail.com>
2022-03-23 13:21:08 -0700

    1.1-1.2: Create functional compoments with input data passed through props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Multiple functional components.
+// Input data passed through props.
+
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>{props.headertext}</h1>
+  )
+};
+
+const Content = (props) => {
+  return (
+      <>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {props.part1} {props.exercises1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          {props.part2} {props.exercises2}
+        </p>
+        <p>
+          {props.part3} {props.exercises3}
+        </p>
+      </>
+  )
+};
+
+const Total = (props) => {
+  return (
+      <p>
+        Number of exercises = {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+  )
+}
+
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+      <div>
+        <Header headertext = {course}/>
+        <Content part1 = {part1} exercises1 = {exercises1}
+                 part2 = {part2} exercises2 = {exercises2}
+                 part3 = {part3} exercises3 = {exercises3}/>
+        <Total exercises1 = {exercises1}
+               exercises2 = {exercises2}
+               exercises3 = {exercises3}/>
+      </div>
+  )
 }
 
 export default App;
