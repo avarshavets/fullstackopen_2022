8792e4548ac1e630715f4787215661e6c979e5b7
Anna Varshavets <a.varshavets@gmail.com>
2022-03-23 17:02:20 -0700

    1.3-1.4: Modifying input data as an array of objects
    Use map() and reduce() to return the sum of the elements in the array.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,57 +1,82 @@
 import './App.css';
-
+// 1.1-1.2
 // Multiple functional components.
 // Input data passed through props.
+// 1.3-1.4
+// Modifying input data as an array of objects.
+// Use map() and reduce() to return the sum of the elements in the array.
 
 const Header = (props) => {
   return (
-      <h1>{props.headertext}</h1>
+      <h1>{props.course.name}</h1>
   )
 };
 
 const Content = (props) => {
   return (
       <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+          {/*<p>*/}
+          {/*  {props.parts[0].name} {props.parts[0].exercises}*/}
+          {/*</p>*/}
+          {/*<p>*/}
+          {/*    {props.parts[1].name} {props.parts[1].exercises}*/}
+          {/*</p>*/}
+          {/*<p>*/}
+          {/*    {props.parts[2].name} {props.parts[2].exercises}*/}
+          {/*</p>*/}
+
+          {/* Alternatively -- Create a list of paragraphs using map() */}
+          {props.course.parts.map(item => <p>{item.name} {item.exercises}</p>)}
       </>
   )
 };
 
 const Total = (props) => {
-  return (
-      <p>
-        Number of exercises = {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-  )
+    // function totalSum() {
+    //     let result = 0;
+    //     for (let i = 0; i < props.parts.length; i++) {
+    //         result += props.parts[i].exercises
+    //     }
+    //     return result
+    // }
+    // ----- Alternatively ---------------------------------------------
+    // create a new array that consists of only exercises by using map()
+    // call reduce() on a newly created array of exercises
+    const totalSum = props.course.parts.map(item => item.exercises).reduce(
+        (prev, cur) => prev + cur, 0
+    )
+    return (
+        <p>
+            Number of exercises = {totalSum}
+        </p>
+    )
 }
 
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+      name: 'Half Stack application development',
+      parts: [
+          {
+              name: 'Fundamentals of React',
+              exercises: 10
+          },
+          {
+              name: 'Using props to pass data',
+              exercises: 7
+          },
+          {
+              name: 'State of a component',
+              exercises: 14
+          }
+          ]
+  }
 
   return (
       <div>
-        <Header headertext = {course}/>
-        <Content part1 = {part1} exercises1 = {exercises1}
-                 part2 = {part2} exercises2 = {exercises2}
-                 part3 = {part3} exercises3 = {exercises3}/>
-        <Total exercises1 = {exercises1}
-               exercises2 = {exercises2}
-               exercises3 = {exercises3}/>
+        <Header course = {course}/>
+        <Content course = {course}/>
+        <Total course = {course}/>
       </div>
   )
 }
