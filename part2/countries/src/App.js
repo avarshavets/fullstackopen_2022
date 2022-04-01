1f9c0d007f789e900e680ffe1f9f86df64e6d9cf
Anna Varshavets <a.varshavets@gmail.com>
2022-03-31 19:05:24 -0700

    Search for a country, display list of matches with 'show' button, or show the country's view if a full match.
    (1) create input field to search for a country and display matches (countries data is fetched with a REST API)
    (2) add 'show' button to a list of matched countries. When clicked, country's info is displayed
    (3) add axios fetch to get additional info about the current weather in the matched country

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,99 +1,147 @@
-// import Course from "./Course";
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 
-// First the body is executed and App component is rendered first time
-// useEffect is executed right after rendering:
-//  - 1st parameter - effect itselt (function that fetches data in this case)
-//  - 2d parameter - specifies how often the effect is run:
-//  --> empty array --> runs once after first render
-//
+
+const ListItem = ({itemValue}) => (
+    <li>{itemValue}</li>
+)
+
+
+const Filter = ({filterText, handleFilterClick, handleFilterChange}) => (
+    <div>
+        find countries <input value={filterText}
+                              onClick={handleFilterClick}
+                              onChange={handleFilterChange}/>
+    </div>
+)
+
+
+const Button = ({id, handleClick}) => (
+    <button id={id} onClick={handleClick}>show</button>
+    )
+
+
+const DisplayOneCountry = ({countryObj}) => {
+    const [weatherObj, setWeather] = useState('')
+
+    // latitude and longitude of the country's capital
+    const [lat, lng] = countryObj.capitalInfo.latlng
+
+    useEffect(() => {
+        const apiKey = process.env.REACT_APP_API_KEY
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}`)
+            .then(response => {
+                setWeather(response.data)})
+    }, [])
+    // [lat, lng] in deps of useEffect -->
+    // fetch weather json object only if lat, lng changes
+
+
+    const languageKeyValuePairs = (Object.entries(countryObj.languages))
+    return (
+        <>
+            <h2>{countryObj.name.common}</h2>
+            <div>
+                <div>capital {countryObj.capital[0]}</div>
+                <div>area {countryObj.area}</div>
+            </div>
+            <h4>languages:</h4>
+            <ul>
+                {languageKeyValuePairs.map(pair =>
+                    <ListItem key={pair[0]} itemValue={pair[1]}/>)}
+            </ul>
+            <img src={countryObj.flags.png} alt='flag'/>
+            <h3>Weather in {countryObj.name.common}</h3>
+            {/* remember:
+            weatherObj will be fetched only after useEffect is executed,
+            which runs after rendering the page content --> thus, there is conditional rendering */}
+            {weatherObj &&
+                <>
+                    <div>
+                        temperature {(weatherObj.main.temp - 273.15).toFixed(2)} Celsius
+                    </div>
+                    <img src={`http://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`} alt='weather'/>
+                    <div>wind {weatherObj.wind.speed} m/s</div>
+                </>
+            }
+        </>
+    )
+}
+
+
+
+const DisplayCountries = ({ filteredCountries, filterText, handleShowClick }) => {
+    if (filteredCountries.length === 0 && filterText) {
+        return <div>no match</div>
+    }
+    if (filteredCountries.length > 10 && filterText) {
+        return (<div>Too many matches, specify another filter.</div>)
+    }
+    if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+        return (
+            <div>
+                {filteredCountries.map(country =>
+                    <p key={country.name.common}>
+                        {country.name.common}<span> </span>
+                        <Button id={country.name.common}
+                                handleClick={handleShowClick}/>
+                    </p>
+                )}
+            </div>
+        )
+    }
+    if (filteredCountries.length === 1) {
+        return <DisplayOneCountry countryObj={filteredCountries[0]}/>
+    }
+    return <></>
+}
+
 
 const App = () => {
-  // const courses = [
-  //   {
-  //     name: 'Half Stack application development',
-  //     id: 1,
-  //     parts: [
-  //       {
-  //         name: 'Fundamentals of React',
-  //         exercises: 10,
-  //         id: 1
-  //       },
-  //       {
-  //         name: 'Using props to pass data',
-  //         exercises: 7,
-  //         id: 2
-  //       },
-  //       {
-  //         name: 'State of a component',
-  //         exercises: 14,
-  //         id: 3
-  //       },
-  //       {
-  //         name: 'Redux',
-  //         exercises: 11,
-  //         id: 4
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Node.js',
-  //     id: 2,
-  //     parts: [
-  //       {
-  //         name: 'Routing',
-  //         exercises: 3,
-  //         id: 1
-  //       },
-  //       {
-  //         name: 'Middlewares',
-  //         exercises: 7,
-  //         id: 2
-  //       }
-  //     ]
-  //   }
-  // ]
+    const [countries, setCountries] = useState([])
+    const [filterText, setFilterText] = useState('')
 
-  const [notes, setNotes] = useState([])
-  // const [newNote, setNewNote] = useState('')
-  // const [showAll, setShowAll] = useState(true)
+    // countryToShow captures the id of a button (onClick trigger) in a paragraph,
+    // which is associated with a country in a filtered list of countries
+    const [countryToShow, setCountryToShow] = useState('')
 
-  const fetchData = () => {
-    console.log('effect')
-
-    const eventHandler = response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
+    const handleFilterChange = (event) => {
+        setFilterText(event.target.value)
+        // reset countryToShow
+        setCountryToShow('')
+    }
+    const handleFilterClick = (e) => {
+        e.target.value = ''
+    }
+    const handleShowClick = (e) => {
+        setCountryToShow(e.target.getAttribute('id'))
     }
 
-    const promise = axios.get('http://localhost:3002/notes')
-    promise.then(eventHandler)
-  }
+    // Get data about countries via a RESTful API
+    useEffect(() => {
+        axios.get('https://restcountries.com/v3.1/all/')
+            .then(response => {
+                setCountries(response.data)
+            })
+    }, [])
 
-  // executing fetchData effect with useEffect()
-  useEffect(fetchData, [])
-  console.log('render', notes.length, 'notes')
+    // if countryToShow is not empty, use it as a final filter text
+    // remember: using setFilterText will cause an infinite loop due to infinite re-rendering
+    const finalFilterText = countryToShow ? countryToShow : filterText
 
-  // ----- a more compact way of definition -----
-  useEffect(() => {
-    console.log('effect')
-    axios
-        .get('http://localhost:3002/notes')
-        .then(response => {
-          console.log('promise fulfilled')
-          setNotes(response.data)
-        })
-  }, [])
-  console.log('render', notes.length, 'notes')
+    const filteredCountries = countries.filter(country =>
+        country.name['common'].toLowerCase().includes(finalFilterText.toLowerCase()))
 
-  return (
-      <>
-        <h1>Notes</h1>
-        {/*{courses.map(courseObj =>*/}
-        {/*    <Course key={courseObj.id} course={courseObj}/>*/}
-        {/*)}*/}
-      </>
-  )
+    return (
+        <>
+            <Filter filterText={filterText}
+                    handleFilterClick={handleFilterClick}
+                    handleFilterChange={handleFilterChange} />
+            <DisplayCountries filteredCountries={filteredCountries}
+                              filterText={finalFilterText}
+                              handleShowClick={handleShowClick}/>
+        </>
+    )
 }
 export default App;
