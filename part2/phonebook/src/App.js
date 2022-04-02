3e810f8d7809c5e865b333035a0f832dfee72c74
Anna Varshavets <a.varshavets@gmail.com>
2022-04-01 23:56:30 -0700

    Expand phonebook app by adding functionalities to communicate with a backend server: save, delete, or update a data entry

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,25 @@
 import axios from "axios";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import personService from './services/person_service'
 
 
-const PersonInfo = ({person}) => (
-        <p>{person.name} {person.num}</p>
+const PersonInfo = ({person, removePerson} ) => (
+    <p>
+        {person.name} {person.num}<span> </span>
+        <button onClick={() => {removePerson(person)} }>
+            delete
+        </button>
+    </p>
     )
 
 
-const DisplayPersons = ({ persons, filterText }) => {
-    const filteredPersons = persons.filter(person =>
-        person.name.toLowerCase().includes(filterText.toLowerCase()))
-    return (
+const DisplayPersons = ({ filteredPersons, removePerson }) => (
         <div>
             {filteredPersons.map(item =>
-                <PersonInfo key={item.name} person={item}/>)}
+                <PersonInfo key={item.id} person={item} removePerson={removePerson} />
+            )}
         </div>
     )
-}
 
 
 const Filter = ({filterText, handleFilterChange}) => (
@@ -27,7 +30,7 @@ const Filter = ({filterText, handleFilterChange}) => (
 )
 
 
-const PersonForm = ({persons, addToPersons}) => {
+const PersonForm = ({addToPersons}) => {
     const [newName, setNewName] = useState('')
     const [newNum, setNewNum] = useState('')
 
@@ -37,8 +40,8 @@ const PersonForm = ({persons, addToPersons}) => {
     const addPerson = (event) => {
         event.preventDefault()
         const personObj = {
-            name: newName.trim().toLowerCase(),
-            num: newNum.trim().toLowerCase()
+            name: newName.trim(),
+            num: newNum.trim()
         }
 
         addToPersons(personObj)
@@ -71,23 +74,52 @@ const App = () => {
     const [filterText, setFilterText] = useState('')
     const handleFilterChange = (event) => setFilterText(event.target.value)
 
-    // add person object to state piece 'persons' if doesn't exist yet
+    // filtered list of persons to be displayed depending on search filter
+    const filteredPersons = persons.filter(person =>
+        person.name.toLowerCase().includes(filterText.toLowerCase()))
+
+    // if persons name exists, confirm update (True && update action),
+    // else add person object to state piece 'persons'
     // reference: comparing the Obj in JS [https://www.joshbritz.co/posts/why-its-so-hard-to-check-object-equality/]
-    const addToPersons = (personObj) => {
-        // remember: concat() creates a new array
-        persons.map(person =>
-            person.name).includes(personObj.name) ?
-            alert(`${personObj.name} is already added to the phonebook`) :
-            setPersons(persons.concat(personObj))
+    const addToPersons = personObj => {
+        const personObjFound = persons.find(person =>
+            person.name.toLowerCase() === personObj.name.toLowerCase())
+
+        personObjFound ?
+            window.confirm(`${personObjFound.name} is already added to the phonebook, 
+            replace the old number with a new one?`) &&
+            updatePerson(personObjFound.id, personObj) :
+
+            // save new personObj to a backend server and update 'persons' state
+            personService
+                .create(personObj)
+                .then(returnedObj => {
+                    setPersons(persons.concat(returnedObj))
+                })
+    }
+
+    const removeFromPersons = personObj => {
+        window.confirm(`Delete ${personObj.name}?`) &&
+        personService
+            .remove(personObj.id)
+            .then(() => {
+                setPersons(persons.filter(p => p.id !== personObj.id))
+            })
+    }
+
+    const updatePerson = (personId, newPersonObj) => {
+        personService
+            .update(personId, newPersonObj)
+            .then(returnedObj => {
+                setPersons(persons.map(p => p.id !== personId ? p : returnedObj))
+            })
     }
 
     // effect - fetching data from json file
     useEffect(() => {
-        axios
-            .get('http://localhost:3002/persons')
-            .then((response) => {
-                setPersons(response.data)
-            })
+        personService
+            .getAll()
+            .then(data => {setPersons(data)})
     }, [])
 
     return (
@@ -95,10 +127,10 @@ const App = () => {
             <h2>Phonebook</h2>
             <Filter filterText={filterText} handleFilterChange={handleFilterChange}/>
             <h3>add a new</h3>
-            <PersonForm persons={persons}
-                        addToPersons={addToPersons}/>
+            <PersonForm addToPersons={addToPersons}/>
             <h3>Numbers</h3>
-            <DisplayPersons persons={persons} filterText={filterText}/>
+            <DisplayPersons filteredPersons={filteredPersons}
+                            removePerson={removeFromPersons}/>
         </div>
     )
 }
