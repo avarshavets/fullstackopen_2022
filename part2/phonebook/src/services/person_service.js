f638e88101597afb471ededbb6649624e8c84a0f
Anna Varshavets <a.varshavets@gmail.com>
2022-04-01 23:44:46 -0700

    Create a service (a group of APIs) to communicate with a backend server

--- part2/phonebook/src/services/person_service.js
+++ part2/phonebook/src/services/person_service.js
@@ -4,5 +4,20 @@ const baseUrl = 'http://localhost:3002/persons'
 
 const getAll = () => {
     const promise = axios.get(baseUrl)
-    return promise.then(response => {response.data})
+    return promise.then(response => response.data)
 }
+
+const create = newObj => {
+    const promise = axios.post(baseUrl, newObj)
+    return promise.then(response => response.data)
+}
+
+const update = (id, newObj) => {
+    const promise = axios.put(`${baseUrl}/${id}`, newObj)
+    return promise.then(response => response.data)
+}
+
+const remove = (id) => axios.delete(`${baseUrl}/${id}`)
+
+
+export default { getAll, create, update, remove }
