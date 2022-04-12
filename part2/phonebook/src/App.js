086930513768db7484964ab1a0eb5e1ad0e5d39a
Anna Varshavets <a.varshavets@gmail.com>
2022-04-11 21:09:41 -0700

    Add validation in backend and reflect in frontend

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,26 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import personService from './services/person_service'
 
 
-const PersonInfo = ({person, removePerson} ) => (
+const PersonInfo = ({person, removePerson}) => (
     <p>
         {person.name} {person.num}<span> </span>
-        <button onClick={() => {removePerson(person)} }>
+        <button onClick={() => {
+            removePerson(person)
+        }}>
             delete
         </button>
     </p>
-    )
+)
 
 
-const DisplayPersons = ({ filteredPersons, removePerson }) => (
-        <div>
-            {filteredPersons.map(item =>
-                <PersonInfo key={item.id} person={item} removePerson={removePerson} />
-            )}
-        </div>
-    )
+const DisplayPersons = ({filteredPersons, removePerson}) => (
+    <div>
+        {filteredPersons.map(item =>
+            <PersonInfo key={item.id} person={item} removePerson={removePerson}/>
+        )}
+    </div>
+)
 
 
 const Filter = ({filterText, handleFilterChange}) => (
@@ -33,7 +35,9 @@ const PersonForm = ({addToPersons}) => {
     const [newName, setNewName] = useState('')
     const [newNum, setNewNum] = useState('')
 
-    const handleNameChange = (event) => {setNewName(event.target.value)}
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
     const handleNumChange = (event) => setNewNum(event.target.value)
 
     const addPerson = (event) => {
@@ -58,7 +62,7 @@ const PersonForm = ({addToPersons}) => {
             </div>
             <div>
                 number: <input value={newNum} placeholder={'add a phone number ...'}
-                             onChange={handleNumChange} required/>
+                               onChange={handleNumChange} required/>
             </div>
             <div>
                 <button type="submit">add</button>
@@ -71,8 +75,7 @@ const PersonForm = ({addToPersons}) => {
 const App = () => {
     const [persons, setPersons] = useState([])
     const [filterText, setFilterText] = useState('')
-    const [successMsg, setSuccessMsg] = useState(null)
-    const [errorMsg, setErrorMsg] = useState(null)
+    const [notification, setNotification] = useState(null)
 
     const handleFilterChange = (event) => setFilterText(event.target.value)
 
@@ -80,33 +83,49 @@ const App = () => {
     const filteredPersons = persons.filter(person =>
         person.name.toLowerCase().includes(filterText.toLowerCase()))
 
+    const notify = (message, type = 'info') => {
+        setNotification({message, type}) //{message: message, type: type}
+        setTimeout(() => {
+            setNotification(null)
+        }, 3000)
+    }
+
     // if persons name exists, confirm update (True && update action),
     // else add person object to state piece 'persons'
     // reference: comparing the Obj in JS [https://www.joshbritz.co/posts/why-its-so-hard-to-check-object-equality/]
     const addToPersons = personObj => {
-        const personObjFound = persons.find(person =>
-            person.name.toLowerCase() === personObj.name.toLowerCase())
-
-        const showSuccessMsg = () => {
-            setSuccessMsg(`Added ${personObj.name}`)
-            setTimeout(() => {setSuccessMsg(null)}, 2000)
-        }
-
-        if (personObjFound) {
-            if (window.confirm(`${personObjFound.name} is already added to the phonebook, 
-            replace the old number with a new one?`)) {
-                updatePerson(personObjFound.id, personObj)
-                showSuccessMsg()
-            }
-        } else {
-            // save new personObj to a backend server and update 'persons' state
-            personService
+        // const personObjFound = persons.find(person =>
+        //     person.name.toLowerCase() === personObj.name.toLowerCase())
+        //
+        // if (personObjFound) {
+        //     if (window.confirm(`${personObjFound.name} is already added to the phonebook,
+        //     replace the old number with a new one?`)) {
+        //         updatePerson(personObjFound.id, personObj)
+        //         notify(`Updated info of ${personObj.name}`)
+        //     }
+        // } else {
+        //     // save new personObj to a backend server and update 'persons' state
+        //     personService
+        //         .create(personObj)
+        //         .then(returnedObj => {
+        //             setPersons(persons.concat(returnedObj))
+        //             notify(`Added ${personObj.name}`)
+        //         })
+        //         .catch(error => {
+        //             console.log(error.response.data)
+        //             notify(error.response.data.error, 'alert')
+        //         })
+        // }
+        personService
                 .create(personObj)
                 .then(returnedObj => {
                     setPersons(persons.concat(returnedObj))
+                    notify(`Added ${personObj.name}`)
                 })
-            showSuccessMsg()
-        }
+                .catch(error => {
+                    console.log(error.response.data)
+                    notify(error.response.data.error, 'alert')
+                })
     }
 
     const removeFromPersons = personObj => {
@@ -117,9 +136,8 @@ const App = () => {
                 setPersons(persons.filter(p => p.id !== personObj.id))
             })
             .catch(error => {
-                setErrorMsg(`Information of ${personObj.name} 
-                has been already removed`)
-                setTimeout(() => {setErrorMsg(null)}, 5000)
+                notify(`Information of ${personObj.name} 
+                has been already removed`, 'alert')
             })
     }
 
@@ -129,56 +147,46 @@ const App = () => {
             .then(returnedObj => {
                 setPersons(persons.map(p => p.id !== personId ? p : returnedObj))
             })
+            .catch(error => {
+                console.log(error.response.data)
+                notify(error.response.data, 'alert')
+            })
     }
 
     // effect - fetching data from json file
     useEffect(() => {
         personService
             .getAll()
-            .then(data => {setPersons(data)})
+            .then(data => {
+                setPersons(data)
+            })
     }, [])
 
     // notification of success when adding a person
-    const SuccessNotification = ({msg}) => {
+    const Notification = ({notification}) => {
+        if (notification === null) {
+            return null
+        }
         // css properties for successMsg as JS object
-        const successMsgStyle = {
-            color: 'green',
+        const style = {
+            color: notification.type === 'info' ? 'green' : 'red',
             backgroundColor: 'lightgrey',
             fontSize: 16,
             borderStyle: 'solid',
-            borderColor: 'green',
+            borderColor: notification.type === 'info' ? 'green' : 'red',
             birderRadius: 5,
             padding: 10,
             marginBottom: 10
         }
-        if (msg === null) {return null}
         return (
-            <div style={successMsgStyle}>{msg}</div>
-        )
-    }
-
-    const ErrorNotification = ({msg}) => {
-        const errorMsgStyle = {
-            color: 'red',
-            backgroundColor: 'lightgrey',
-            fontSize: 16,
-            borderStyle: 'solid',
-            borderColor: 'red',
-            birderRadius: 5,
-            padding: 10,
-            marginBottom: 10
-        }
-        if (msg === null) {return null}
-        return (
-            <div style={errorMsgStyle}>{msg}</div>
+            <div style={style}>{notification.message}</div>
         )
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <SuccessNotification msg={successMsg}/>
-            <ErrorNotification msg={errorMsg}/>
+            <Notification notification={notification}/>
             <Filter filterText={filterText} handleFilterChange={handleFilterChange}/>
             <h3>add a new</h3>
             <PersonForm addToPersons={addToPersons}/>
