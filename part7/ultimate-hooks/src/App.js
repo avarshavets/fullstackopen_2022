fede7674120522558d04a622e03b535a7ab3926c
Anna Varshavets <a.varshavets@gmail.com>
2022-05-11 19:52:46 -0700

    Add custom hook useResources that allows to reuse code related to communication with the json-db server for notes and for persons resources

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -15,19 +15,39 @@ const useField = (type) => {
   }
 }
 
+// code for communication with the json-db server as a custom hook function
+// purpose of this hook to encapsulate reusable code that is used for notes and persons
+// without the useResource code for service must be duplicated for notes and persons
+// (as notes_services and persons_services)
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = () => {
+    axios.get(baseUrl).then(response => setResources(response.data))
+  }
 
+  // resource is an object to be added to json object list (nodes or persons)
   const create = (resource) => {
-    // ...
+    axios
+        .post(baseUrl, resource)
+        .then(returnedObj => {
+          setResources(resources.concat(returnedObj))
+        })
   }
 
   const service = {
+    getAll,
     create
   }
 
+  // we can also return an object with 2 key-value pairs
+  // however, when we use this hook in the App,
+  // we use deconstruction of parameters and assign then to variables with different name as:
+  // const [notes, noteService] = useResource('http://localhost:3005/notes')
+  // we wouldn't be able to rename the variables when using an object.
+  // in case of an object it would have been the following:
+  // const notes = useResources(...)
+  // where notes.resources - list of notes, notes.service - object of functions
   return [
     resources, service
   ]
@@ -37,9 +57,14 @@ const App = () => {
   const content = useField('text')
   const name = useField('text')
   const number = useField('text')
-
+  // const a_list = []
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+  }, [notes, persons])
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
