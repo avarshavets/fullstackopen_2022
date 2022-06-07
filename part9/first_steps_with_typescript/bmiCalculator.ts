d360ff81121fc646891f4619f6021ff4fd4975cf
Anna Varshavets <a.varshavets@gmail.com>
2022-06-07 14:03:52 -0700

    Simple functions in Typescript

---
+++ part9/first_steps_with_typescript/bmiCalculator.ts
@@ -0,0 +1,58 @@
+type Result = string
+const calculateBmi = (height: number, weight: number): Result => {
+    const bmi = weight/((height/100)^2)
+    if (bmi < 18.4) {
+        return 'Underweight'
+    }
+    if (bmi > 18.5 && bmi < 24.9) {
+        return 'Normal'
+    }
+    if (bmi > 25.0 && bmi < 29.9) {
+        return 'Overweight'
+    }
+    if (bmi > 30.0) {
+        return 'Obese'
+    }
+    throw new Error('not being able to perform calculation')
+}
+
+// bmiCalculator.js is run with command "npm run calculateBmi", which is defined in package.json
+console.log(calculateBmi(180, 74))
+
+
+// To get the calculator work with command-line parameters, process.argv
+// Note: argv[0] = "ts-node", argv[1] = "file name", and argv[2] , argv[3] are height and weight respectively!
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+// console.log(`BMI with ${height} and ${weight} =`, calculateBmi(height, weight))
+
+
+// Adding data validation for data provided through command-line
+// Interface defines the "shape" an object should have
+interface Values {
+    value1: number,
+    value2: number
+}
+const parseArguments = (args: Array<string>): Values => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+    if (args.length > 4) throw new Error('Too many arguments')
+
+    // isNaN ~ is Not-A-Number
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    }
+    else {
+        throw new Error('Provided values are not numbers!')
+    }
+}
+
+try {
+    const { value1, value2 } = parseArguments(process.argv)
+    console.log('Calculated Bmi is', calculateBmi(value1, value2))
+}
+catch (error: unknown) {
+    console.log(error)
+}
---
+++ part9/first_steps_with_typescript/exerciseCalculator.ts
@@ -0,0 +1,88 @@
+interface ExerciseResult {
+    periodLength: number,
+    trainingDays: number,
+    target: number,
+    average: number,
+    success: Boolean,
+    rating: number,
+    ratingDescription: string
+}
+
+// daily exercises are given as an array of 7 numbers --> hours of exercise for each day
+// target --> target amount of daily hours
+const calculateExercises = (target: number, exercises: Array<number>): ExerciseResult => {
+    const getRating = () => {
+        if (average < 1) {
+            return 1
+        }
+        if (average < 2) {
+            return 2
+        }
+        if (average >= 2) {
+            return 3
+        }
+    }
+    const ratingExplanation = {
+        1: 'not enough exercises',
+        2: 'not too bad but could be better',
+        3: 'great job'
+    }
+    const periodLength = exercises.length
+    const trainingDays = exercises.reduce((sum, item) =>
+            item !== 0 ? sum + 1 : sum
+    , 0)
+    const average = exercises.reduce((sum, item) => (sum + item), 0) / periodLength
+    const rating = getRating()
+    return {
+        periodLength,
+        trainingDays,
+        target,
+        average,
+        success: average >= target,
+        rating,
+        ratingDescription: ratingExplanation[rating]
+    }
+}
+
+console.log(calculateExercises(2,[3, 0, 2, 4.5, 0, 3, 1]))
+
+// function that takes and parsed the input from the command line
+interface ExerciseInputValues {
+    target: number,
+    exercises: Array<number>
+}
+const parseExerciseArguments = (args: Array<string>) => {
+    // ts-node + file name + target + 7 daily exercise hours --> 10 arguments
+    if (args.length < 10) throw  new Error('Not enough arguments')
+
+    // remove/slice 2 first items from arguments, which refer to run file command (ts-node file_name)
+    const arrayOfStrings = args.slice(2)
+    const areNumbers = () => {
+        for (const s of arrayOfStrings) {
+            if (isNaN(Number(s))) {
+                return false
+            }
+        }
+        return true
+    }
+    if (!areNumbers) throw new Error('Provided values are not numbers!')
+
+    let exercises = []
+    const target = Number(arrayOfStrings[0])
+    for (const s of arrayOfStrings.slice(1)) {
+        exercises.push(Number(s))
+    }
+    return {
+        target,
+        exercises
+    }
+}
+
+// run the calculator
+try {
+    const { target, exercises } = parseExerciseArguments(process.argv)
+    console.log(calculateExercises(target, exercises))
+}
+catch (error: unknown) {
+    console.log(error)
+}
